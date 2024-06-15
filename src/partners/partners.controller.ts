d81c4ee53a7f38a2915722ca7778aa83b2b383f1
Thiago Cedran <thiagocdn@gmail.com>
2024-06-14 21:46:38 -0300

    Created route to create partnet

---
+++ src/partners/partners.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Post, Body, UseGuards, Req } from '@nestjs/common';
+import { PartnersService } from './partners.service';
+import { CreatePartnerDto } from './dto/create-partner.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+
+@UseGuards(AuthGuard)
+@Controller('partners')
+export class PartnersController {
+  constructor(private readonly partnersService: PartnersService) {}
+
+  @Post()
+  create(@Body() createPartnerDto: CreatePartnerDto, @Req() req: any) {
+    return this.partnersService.create({
+      ...createPartnerDto,
+      userId: req.user.id,
+    });
+  }
+
+  // @Get()
+  // findAll() {
+  //   return this.partnersService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.partnersService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updatePartnerDto: UpdatePartnerDto) {
+  //   return this.partnersService.update(+id, updatePartnerDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.partnersService.remove(+id);
+  // }
+}
---
+++ src/partners/partners.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePartnerDto } from './dto/create-partner.dto';
+import { UpdatePartnerDto } from './dto/update-partner.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class PartnersService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createPartnerDto: CreatePartnerDto & { userId: number }) {
+    const partner = await this.prismaService.$transaction(async (prisma) => {
+      const partner = await prisma.partner.create({
+        data: { name: createPartnerDto.name },
+      });
+
+      await prisma.partnerUser.create({
+        data: {
+          userId: createPartnerDto.userId,
+          partnerId: partner.id,
+        },
+      });
+      return partner;
+    });
+
+    return partner;
+  }
+
+  findAll() {
+    return `This action returns all partners`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} partner`;
+  }
+
+  update(id: number, updatePartnerDto: UpdatePartnerDto) {
+    return `This action updates a #${id} partner`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} partner`;
+  }
+}
