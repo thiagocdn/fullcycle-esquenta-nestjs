35de9c328368a6718bc776992ac762e365b5fa65
Thiago Cedran <thiagocdn@gmail.com>
2024-06-14 22:44:34 -0300

    Added interception for roles in routes

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -14,18 +14,23 @@ import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { TenantInterceptor } from 'src/tenant/tenant.interceptor';
+import { RolesGuard } from 'src/auth/roles/roles.guard';
+import { Roles } from 'src/auth/roles/roles.decorator';
+import { UserRoles } from 'src/auth/users/user-roles';
 
 @UseInterceptors(TenantInterceptor)
-@UseGuards(AuthGuard)
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('events')
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
+  @Roles(UserRoles.PARTNER)
   @Post()
   async create(@Body() createEventDto: CreateEventDto) {
     return this.eventsService.create(createEventDto);
   }
 
+  @Roles(UserRoles.PARTNER)
   @Get()
   findAll() {
     return this.eventsService.findAll();
